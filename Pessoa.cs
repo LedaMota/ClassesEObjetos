f3204e09f93c13416721aeeed6eae42aad505421
Samuel Ribeiro Batista <leidymotastz@outlook.com>
2022-09-17 17:24:59 -0300

    First Commit

---
+++ Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_e_objetos
+{
+    class Pessoa
+    {
+        //Atributos / Campos
+        public string nome;
+        public string sobrenome;
+        public int anoNascimento;
+
+        //Métodos Simples
+        public void Cumprimentar()
+        {
+            Console.WriteLine("Ola eu sou " + nome + "" + sobrenome);
+        }
+
+
+    }
+       
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_e_objetos
+{
+    class Program
+         static void Main(string[] args)
+
+         { 
+
+    
+    
+         }
+
+      
+
+
+}   
+
+
+
+
+
+       
+class MinhaClasse
+{
+
+}
+            
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+
+
+
+
+    
+
+
+        
+       
+
+    
+    
+         
+    
+    
+    
+    
+ 
+
+
+
+
+          
